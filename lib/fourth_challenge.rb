7f6bb81a7c053f6a85e5d7c22556d4b401d65ac7
Monica Marziano <mmarziano1@gmail.com>
2018-04-14 21:19:12 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,12 +6,12 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         {
-          :name => "Benvolio"
-          :age => "17"
+          :name => "Benvolio",
+          :age => "17",
           :attitude => "worried"},
-        { :name => "Mercutio"
-          :age => "18"
-          :attitude => "hot-headead"}
+        { :name => "Mercutio",
+          :age => "18",
+          :attitude => "hot-headed"}
           
       ]
    }, 
@@ -20,7 +20,13 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        #your hashes here!
+         {
+          :name => "Steven",
+          :age => "30",
+          :attitude => "confused"},
+        { :name => "Mercutio"
+          :age => "18"
+          :attitude => "hot-headead"}
       ]
    }
 }
